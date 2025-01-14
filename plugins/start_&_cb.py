089cd79b0c09893c38b8f0f9e195002060c1a6c9
blandk77 <alexags1983@gmail.com>
2025-01-15 00:08:51 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -2,11 +2,14 @@ import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
 from helper.database import jishubotz
-from config import Config, Txt  
+from config import Config, Txt 
+from utils import verify_user, check_token
   
 
 @Client.on_message(filters.private & filters.command("start"))
-async def start(client, message):
+async def start(bot, update):
+    client = bot
+    message = update 
     user = message.from_user
     await jishubotz.add_user(client, message)                
     button = InlineKeyboardMarkup([
